3c4decf4a09044ecd2cb6340c144d5ff10f71d12
Rcpdroid <amos.marbun86@gmail.com>
2026-05-18 12:54:55 +0700

    Add functions to open and close modal

---
+++ modal.js
@@ -0,0 +1,7 @@
+window.closeModal = function() {
+  document.getElementById("modal").style.display = "none";
+};
+
+window.openModal = function() {
+  document.getElementById("modal").style.display = "flex";
+};
