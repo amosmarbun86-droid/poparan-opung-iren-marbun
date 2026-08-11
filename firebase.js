e34627e0c043becdd555a26c61b7fc0b80ef48e9
Rcpdroid <amos.marbun86@gmail.com>
2026-08-11 11:15:58 +0700

    Add Firebase Auth import and initialization

--- firebase.js
+++ firebase.js
@@ -4,6 +4,10 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAwb3uKq12i0VKYQnLfpmFnOUyosbcJXUI",
   authDomain: "poparan-opung-iren-marbun.firebaseapp.com",
@@ -13,3 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
